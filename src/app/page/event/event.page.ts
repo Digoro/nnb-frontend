3d0cc8e29f7345d02a89ef18ebbdb6245deb081c
digoro <lsh000124@gmail.com>
2020-08-15 01:24:34 +0900

    set padding event page

--- src/app/page/event/event.page.ts
+++ src/app/page/event/event.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CheckDesktopService } from 'src/app/service/check-desktop.service';
 
 @Component({
   selector: 'event',
@@ -7,12 +8,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./event.page.scss'],
 })
 export class EventPage implements OnInit {
+  isDesktop = true;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private cds: CheckDesktopService
   ) { }
 
   ngOnInit() {
+    this.cds.isDesktop.subscribe(resp => this.isDesktop = resp)
   }
 
   onClick(info: any, isLink: boolean = false) {
