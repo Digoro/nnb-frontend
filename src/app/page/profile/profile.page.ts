90fb25dcbdd7f09d73a95f2a67055e45416ef1b4
Digoro <lsh000124@gmail.com>
2020-11-02 17:40:49 +0900

    fixed

--- src/app/page/profile/profile.page.ts
+++ src/app/page/profile/profile.page.ts
@@ -38,7 +38,7 @@ export class ProfilePage implements OnInit {
         this.user = user;
         this.meetingService.getHostedMeetings(user.uid).subscribe(meetings => {
           this.hostedMeetings = meetings
-          const mids = this.hostedMeetings.filter(meeting => meeting.mid['mid']);
+          const mids = this.hostedMeetings.filter(meeting => meeting.mid);
           mids.forEach(mid => {
             this.commentService.getCommentsByMeeting(+mid).subscribe(comments => {
               this.comments.concat(comments)
