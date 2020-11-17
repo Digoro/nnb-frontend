12ed5c9e1188211b1d7aa91afaeb8ad61d920c16
Digoro <lsh000124@gmail.com>
2020-11-17 17:49:40 +0900

    fixed

--- src/app/page/login/login.page.ts
+++ src/app/page/login/login.page.ts
@@ -36,7 +36,8 @@ export class LoginPage implements OnInit {
         Kakao.init(environment.KAKAO_AUTH_KEY);
       }
       Kakao.Auth.authorize({
-        redirectUri: 'http://nonunbub.com/accounts/kakao/login/callback/',
+        // redirectUri: 'http://localhost:8000/accounts/kakao/login/callback',
+        redirectUri: 'http://nonunbub.com/accounts/kakao/login/callback',
       });
     } else if (method === 'email') {
       alert('서비스 준비중입니다 ^^');
@@ -48,7 +49,8 @@ export class LoginPage implements OnInit {
         gauth.then(() => {
           gauth.signIn({
             ux_mode: 'redirect',
-            redirect_uri: 'http://nonunbub.com/accounts/google/login/callback/',
+            // redirect_uri: 'http://localhost:8000/accounts/google/login/callback',
+            redirect_uri: 'http://nonunbub.com/accounts/google/login/callback',
           })
           console.log('init success');
         }, () => {
