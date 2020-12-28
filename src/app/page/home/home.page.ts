4d32b988311e9aa144d1e404e134b26c01c855e5
digoro <lsh000124@gmail.com>
2020-12-28 16:04:54 +0900

    add host profile

--- src/app/page/home/home.page.ts
+++ src/app/page/home/home.page.ts
@@ -32,6 +32,54 @@ export class HomePage implements OnInit, OnDestroy {
   }
   banners: { image: string, metadata: S3.Metadata }[];
   mainMeetings: { title: string, subTitle: string, onShowKey: string, onShowTitle: string, meetings: Meeting[], emoji?: string, }[]
+  sliderConfig = {
+    observer: true,
+    initialSlide: 0,
+    slidesPerView: 4.1,
+    breakpoints: {
+      922: {
+        slidesPerView: 2.2
+      }
+    },
+    spaceBetween: 10
+  };
+  superHostList = [
+    {
+      uid: '326',
+      name: '이진숙',
+      image: 'https://nonunbub.s3.ap-northeast-2.amazonaws.com/meetings-resized/%EC%9D%B4%EC%A7%84%EC%88%99%20%EA%B0%80%EB%82%98%20%EC%95%84%ED%8A%B8%20%EC%A7%84%ED%96%89%28%EC%82%AC%EC%9D%B4%EC%A6%88%EC%88%98%EC%A0%95%29.jpg',
+      tags: ['도슨트', '미술'],
+      locale: '서울',
+    },
+    {
+      uid: '341',
+      name: '배연하',
+      image: 'https://nonunbub.s3.ap-northeast-2.amazonaws.com/meetings-resized/%EB%B0%B0%EC%97%B0%ED%95%98%EB%8B%98%20%ED%98%B8%EC%8A%A4%ED%8A%B8%EC%86%8C%EA%B0%9C%20%EC%82%AC%EC%A7%84%28%EC%82%AC%EC%9D%B4%EC%A6%88%EC%88%98%EC%A0%95%29.jpg',
+      tags: ['연홍도여행', '힐링'],
+      locale: '전남 고흥',
+    },
+    {
+      uid: '331',
+      name: '김성학',
+      image: 'https://nonunbub.s3.ap-northeast-2.amazonaws.com/meetings-resized/%EA%B9%80%EC%84%B1%ED%95%99%ED%98%B8%EC%8A%A4%ED%8A%B8%EC%86%8C%EA%B0%9C%EC%82%AC%EC%A7%84.jpg',
+      tags: ['남한산성', '둘레길'],
+      locale: '경기 성남',
+    },
+    {
+      uid: '325',
+      name: '변종원',
+      image: 'https://nonunbub.s3.ap-northeast-2.amazonaws.com/meetings-resized/%E1%84%87%E1%85%A7%E1%86%AB%E1%84%8C%E1%85%A9%E1%86%BC%E1%84%8B%E1%85%AF%E1%86%AB%E1%84%92%E1%85%A9%E1%84%89%E1%85%B3%E1%84%90%E1%85%B3%E1%84%82%E1%85%B5%E1%86%B7.jpg',
+      tags: ['목공예', '우든펜'],
+      locale: '서울',
+    },
+    // {
+    //   uid: '0',
+    //   name: '고광자',
+    //   image: 'https://nonunbub.s3.ap-northeast-2.amazonaws.com/meetings/%EA%B3%A0%EA%B4%91%EC%9E%90%20%EB%A9%94%EC%9D%B8%20%EC%82%AC%EC%9D%B4%EC%A6%88%20%EC%88%98%EC%A0%95%20121.jpg',
+    //   tags: ['팜파티', '전통장'],
+    //   locale: '전북 남원',
+    // }
+  ];
 
   constructor(
     private meetingService: MeetingService,
